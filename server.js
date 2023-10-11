40631b68c0f324765d979037d07de2d62e355e6f
tjas98 <t.leghissa@gmail.com>
2023-10-11 15:44:13 +0200

    abc

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ app.get("/*", (req, res) => {
     res.sendFile(__dirname+'idst/novi/index.html')
 });
 
+app.use('/', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 app.listen(process.env.PORT || 8080);
